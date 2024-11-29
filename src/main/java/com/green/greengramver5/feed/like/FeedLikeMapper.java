dc83f962a68ad67d70d046947619e6eba64fc86b
iljikim <kimilji90@gmail.com>
2024-11-29 15:58:57 +0900

    1

--- src/main/java/com/green/greengramver5/feed/like/FeedLikeMapper.java
+++ src/main/java/com/green/greengramver5/feed/like/FeedLikeMapper.java
@@ -1,7 +1,10 @@
 package com.green.greengramver5.feed.like;
 
+import com.green.greengramver5.feed.like.model.FeedLikeReq;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface FeedLikeMapper {
+    int insFeedLike(FeedLikeReq p);
+    int delFeedLike(FeedLikeReq p);
 }
--- src/main/java/com/green/greengramver5/feed/like/FeedLikeService.java
+++ src/main/java/com/green/greengramver5/feed/like/FeedLikeService.java
@@ -1,9 +1,19 @@
 package com.green.greengramver5.feed.like;
 
+import com.green.greengramver5.feed.like.model.FeedLikeReq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class FeedLikeService {
+    private final FeedLikeMapper mapper;
+
+    public int feedLikeToggle(FeedLikeReq p) {
+        int result = mapper.delFeedLike(p);
+        if (result == 0) {
+            return mapper.insFeedLike(p); //좋아요 등록이 되었을 때 return 1
+        }
+        return 0; //좋아요 취소가 되었을 때 return 0
+    }
 }
